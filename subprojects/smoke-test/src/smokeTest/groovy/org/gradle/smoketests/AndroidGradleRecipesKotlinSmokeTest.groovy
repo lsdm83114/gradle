7bb616cdc21e53349bad08edb160847079d9c99f
Justin Van Dort <jvandort@gradle.com>
2024-01-19 15:51:29 -0500

    Expect some deprecation warnings

--- subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AndroidGradleRecipesKotlinSmokeTest.groovy
+++ subprojects/smoke-test/src/smokeTest/groovy/org/gradle/smoketests/AndroidGradleRecipesKotlinSmokeTest.groovy
@@ -208,6 +208,7 @@ class AndroidGradleRecipesKotlinSmokeTest extends AbstractSmokeTest {
             expectBasePluginConventionDeprecation(agpVersion)
             expectBasePluginExtensionArchivesBaseNameDeprecation(kotlinVersionNumber, VersionNumber.parse(agpVersion))
             expectClientModuleDeprecationWarning(agpVersion)
+            expectConfigurationMutationDeprecationWarning(agpVersion, [":app:debugCompileClasspath"])
         }
     }
 }
